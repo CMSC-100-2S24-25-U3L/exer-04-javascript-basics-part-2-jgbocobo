5b7f92fee8712b6f4748896fae7621293c3de47a
jgbocobo <jgbocobo@up.edu.ph>
2025-02-26 11:32:00 +0800

    Added working addAccount function

---
+++ index.js
@@ -0,0 +1,4 @@
+import { addAccount } from "./test.js";
+
+addAccount("james","reid","thisisemail@email.com",20) //valid
+addAccount("kat","anna","thisisemail2@email.com",29) //valid
--- test.js
+++ test.js
@@ -5,3 +5,29 @@ import fs from 'fs';
 function generateUniqueID(x,y){
     return  x.charAt(0).toLowerCase()+y.toLowerCase()+uuid().slice(0,8);   
 }
+
+export function addAccount(name,last,email,age){
+    if(validator.isEmail(email)===true){
+        if(validator.isEmpty(name)===false){
+            if(validator.isEmpty(last)===false){
+                if(age>=18){
+                    let userid=name+","+last+","+email+","+age+","+generateUniqueID(name,last)+"\n";
+                    fs.appendFileSync("users.txt", userid);
+                    return true;
+                }else{
+                    console.log("Age must be 18 or above!");
+                    return false;
+                }
+            }else{
+                console.log("Last name should not be empty!");
+                return false;
+            }
+        }else{
+            console.log("Name should not be empty!");
+            return false;
+        }
+    }else{
+        console.log("Email is invalid!");
+        return false;
+    }
+}
