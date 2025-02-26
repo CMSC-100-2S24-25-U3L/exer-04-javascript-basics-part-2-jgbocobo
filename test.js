f0d9c6494b888264aee3655d85b7b53d8bdd8e2d
jgbocobo <jgbocobo@up.edu.ph>
2025-02-26 11:29:04 +0800

    Created the generateUniqueID function

--- test.js
+++ test.js
@@ -2,3 +2,6 @@ import {v4 as uuid} from 'uuid';
 import validator from 'validator';
 import fs from 'fs';
 
+function generateUniqueID(x,y){
+    return  x.charAt(0).toLowerCase()+y.toLowerCase()+uuid().slice(0,8);   
+}
