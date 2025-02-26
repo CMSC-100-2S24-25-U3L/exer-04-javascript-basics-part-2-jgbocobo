2f7383f737fb8ca7ac450fd912d5f0dabc547596
Jorge Bocobo <159723145+jgbocobo@users.noreply.github.com>
2025-02-26 11:43:46 +0800

    Exer Complete

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ import { addAccount } from "./index.js";
 
 addAccount("james","reid","thisisemail@email.com",20) //valid
 addAccount("kat","anna","thisisemail2@email.com",29) //valid
+addAccount("baby","mwamwa","thisisemail3@email.com",23) //valid
